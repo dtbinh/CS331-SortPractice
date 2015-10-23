27598ef630536b429a183f07897fc43b1192c342
ZhangLiang <zhangliang@21cake.com>
2015-10-23 13:05:25 -0700

    fix script name issue

--- batchTest.sh
+++ batchTest.sh
@@ -1,19 +1,19 @@
-runCmd="php sort_test.php insert $1"
+runCmd="php SortTest.php insert $1"
 echo $runCmd
 $runCmd
 
-runCmd="php sort_test.php merge $1"
+runCmd="php SortTest.php merge $1"
 echo $runCmd
 $runCmd
 
-runCmd="php sort_test.php quick1 $1"
+runCmd="php SortTest.php quick1 $1"
 echo $runCmd
 $runCmd
 
-runCmd="php sort_test.php quick2 $1"
+runCmd="php SortTest.php quick2 $1"
 echo $runCmd
 $runCmd
 
-runCmd="php sort_test.php quick3 $1"
+runCmd="php SortTest.php quick3 $1"
 echo $runCmd
 $runCmd
